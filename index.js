5cf02dad28a6ed38e911a597013d1c171ffe9681
Ayoko Tolu <ayokojoja@gmail.com>
2023-02-02 12:14:59 -1200

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// function subtract(a,b) {
+//     return a - b
+// }
+// console.log (subtract(10,8));
+// function add (num1, num2, num3)
+ num1 = 6;
+num2 = 9;
+num3 = 7;
+num4 = num1 + num2 + num3;
+ console.log(num4)
